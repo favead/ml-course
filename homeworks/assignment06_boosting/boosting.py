80624046652c77ccf457d1e3e49f0d90373e9e7a
favead <diablo3_monk@mail.ru>
2023-11-27 17:06:32 +0300

    Solve 7 homework

--- homeworks/assignment06_boosting/boosting.py
+++ homeworks/assignment06_boosting/boosting.py
@@ -1,35 +1,51 @@
-import numpy as np
+from typing import TypeVar
 
+import numpy as np
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.linear_model import LinearRegression
+
+
+SelfShape = TypeVar("SelfShape", bound="SimplifiedBoostingRegressor")
+
 
 class SimplifiedBoostingRegressor:
     def __init__(self):
         pass
-        
+
     @staticmethod
     def loss(targets, predictions):
-        loss = np.mean((targets - predictions)**2)
+        loss = np.mean((targets - predictions) ** 2)
         return loss
-    
+
     @staticmethod
     def loss_gradients(targets, predictions):
-        gradients = # YOUR CODE HERE
+        gradients = -2 * np.mean(
+            targets[:, None] - predictions[:, None], axis=1
+        )
         assert gradients.shape == targets.shape
         return gradients
-        
-        
-    def fit(self, model_constructor, data, targets, num_steps=10, lr=0.1, max_depth=5, verbose=False):
-        '''
+
+    def fit(
+        self,
+        model_constructor: callable,
+        data: np.ndarray,
+        targets: np.ndarray,
+        num_steps: int = 10,
+        lr: float = 0.1,
+        max_depth: int = 5,
+        verbose: bool = False,
+    ) -> SelfShape:
+        """
         Fit sequence of models on the provided data.
-        Model constructor with no parameters (and with no ()) is passed to this function.
-        If 
-        
+        Model constructor with no parameters (and with no ())
+        is passed to this function.
+        If
+
         example:
-        
-        boosting_regressor = SimplifiedBoostingRegressor()    
+
+        boosting_regressor = SimplifiedBoostingRegressor()
         boosting_regressor.fit(DecisionTreeRegressor, X, y, 100, 0.5, 10)
-        '''
+        """
         new_targets = targets
         self.models_list = []
         self.lr = lr
@@ -38,20 +54,20 @@ class SimplifiedBoostingRegressor:
             try:
                 model = model_constructor(max_depth=max_depth)
             except TypeError:
-                print('max_depth keyword is not found. Ignoring')
+                print("max_depth keyword is not found. Ignoring")
                 model = model_constructor()
             self.models_list.append(model.fit(data, new_targets))
             predictions = self.predict(data)
+            self.last_pred = predictions
             self.loss_log.append(self.loss(targets, predictions))
             gradients = self.loss_gradients(targets, predictions)
-            new_targets = # YOUR CODE HERE
+            new_targets = -gradients
         if verbose:
-            print('Finished! Loss=', self.loss_log[-1])
+            print("Finished! Loss=", self.loss_log[-1])
         return self
-            
-    def predict(self, data):
+
+    def predict(self, data: np.ndarray) -> np.ndarray:
         predictions = np.zeros(len(data))
         for model in self.models_list:
-            predictions += # YOUR CODE HERE
+            predictions += self.lr * model.predict(data)
         return predictions
-
